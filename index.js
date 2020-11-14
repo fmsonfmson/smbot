4ae9ebec9b48083baf3200ee5c5cdcb7b7a04850
fmsonfmson <fmosnfmson@naver.com>
2020-11-14 12:09:28 +0900

    token

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = 'Nzc2OTk3MjcyODUxOTcyMTA2.X69BGQ.PpwWJa2c41uqRaXc8Fx4GnNzQII';
+const token = precess.env.token;
 client.on('ready', () => {
     console.log('on');
 });
